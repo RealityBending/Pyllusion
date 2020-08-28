c3d05b59e9e830f2a8b4de40fbe17c7796a04cec
DominiqueMakowski <dom.mak19@gmail.com>
2020-08-28 09:51:43 +0800

    add simultaneous contrasts illusion

--- pyllusion/illusion/__init__.py
+++ pyllusion/illusion/__init__.py
@@ -8,4 +8,5 @@ from .ponzo import ponzo_parameters, ponzo_image
 from .rodframe import rodframe_parameters, rodframe_image
 from .mullerlyer import mullerlyer_parameters, mullerlyer_image
 from .verticalhorizontal import verticalhorizontal_parameters, verticalhorizontal_image
+from .contrast import contrast_parameters, contrast_image
 from .autostereogram import autostereogram
---
+++ pyllusion/illusion/contrast.py
@@ -0,0 +1,84 @@
+import numpy as np
+import PIL.Image, PIL.ImageDraw, PIL.ImageFilter, PIL.ImageFont, PIL.ImageOps
+from ..image import image_rectangle, rescale
+
+def contrast_image(parameters=None, width=800, height=600, **kwargs):
+    """
+    Examples
+    ---------
+    >>> import pyllusion as ill
+    >>>
+    >>> parameters = ill.contrast_parameters(difference=0, illusion_strength=-50)
+    >>> ill.contrast_image(parameters)
+    """
+    # Create white canvas and get drawing context
+    if parameters is None:
+        parameters = contrast_parameters(**kwargs)
+
+    # Background upper
+    image  = PIL.Image.new('RGB', (width, height), color=parameters["Background_Upper_RGB"])
+    # Background lower
+    image = image_rectangle(image=image, y=-0.5, size_height=1, size_width=2, color=parameters["Background_Lower_RGB"], antialias=False)
+
+    image = image_rectangle(image=image, y=0.5, size_height=0.5, size_width=1, color=parameters["Rectangle_Upper_RGB"], antialias=False)
+    image = image_rectangle(image=image, y=-0.5, size_height=0.5, size_width=1, color=parameters["Rectangle_Lower_RGB"], antialias=False)
+
+    return image
+
+
+def contrast_parameters(difference=0, illusion_strength=0):
+
+    # Targets
+    # A difficulty of 1 means a difference of 1%
+    up = 0.5 + difference / 200
+    low = 0.5 - difference / 200
+
+    # Backgrounds
+    # A illusion_strength of 1 means a difference of 1%
+    mod = np.sign(difference) if difference != 0 else 1
+    background_up = 0.5 - mod * illusion_strength / 200
+    background_low = 0.5 + mod * illusion_strength / 200
+
+    # Adjustments in case of same contrast between rectangle and background
+    if background_up == up:
+        if background_up > background_low:
+            background_up += 0.01
+        elif background_up < background_low:
+            background_up -= 0.01
+        else:
+            background_up -= 0.01
+            background_low -= 0.01
+    if background_low == low:
+        if background_low > background_up:
+            background_low += 0.01
+        elif background_low < background_up:
+            background_low -= 0.01
+        else:
+            background_low -= 0.01
+            background_up -= 0.01
+
+
+    # Transform to RGB tuples
+    background_up_rgb = tuple(np.rint(rescale([background_up]*3, scale=[0, 1], to=[0, 256])).astype(int))
+    background_low_rgb = tuple(np.rint(rescale([background_low]*3, scale=[0, 1], to=[0, 256])).astype(int))
+    up_rgb = tuple(np.rint(rescale([up]*3, scale=[0, 1], to=[0, 256])).astype(int))
+    low_rgb = tuple(np.rint(rescale([low]*3, scale=[0, 1], to=[0, 256])).astype(int))
+
+    parameters = {
+        "Illusion": "Contrast",
+        "Illusion_Strength": illusion_strength,
+        "Difference": difference,
+        "Illusion_Type": "Congruent" if illusion_strength > 0 else "Incongruent",
+
+        "Background_Upper": background_up,
+        "Background_Lower": background_low,
+        "Rectangle_Upper": up,
+        "Rectangle_Lower": low,
+
+        "Background_Upper_RGB": background_up_rgb,
+        "Background_Lower_RGB": background_low_rgb,
+        "Rectangle_Upper_RGB": up_rgb,
+        "Rectangle_Lower_RGB": low_rgb
+    }
+
+    return parameters
