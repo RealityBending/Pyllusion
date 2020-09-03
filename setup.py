f9c92668e9f7994d23af4e6bd8b6684bd189322b
Tam Pham <phamttam17@gmail.com>
2020-09-03 12:49:33 +0800

    fix test setup

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ setup(
     package_data = {
         "pyllusion.stimuli":["*.ai"],
         "pyllusion.stimuli":["*.png"]},
-    install_requires = dependencies,
     dependency_links=[],
     long_description = open('README.md', encoding='utf8').read(),
     keywords = "python pyllusion visual optical illusions",
