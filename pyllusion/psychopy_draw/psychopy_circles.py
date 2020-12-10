628cc8aa86ffe1972a9988964813fab7130712c9
Zen <zen-juen@users.noreply.github.com>
2020-12-10 11:59:48 +0800

    circle outline

--- pyllusion/psychopy_draw/psychopy_circles.py
+++ pyllusion/psychopy_draw/psychopy_circles.py
@@ -9,8 +9,8 @@ def psychopy_circle(
     y=0,
     size=1,
     color="black",
-    outline=0,
-    color_outline="black",
+    outline=1,
+    color_outline="copy",
     blur=0,
     antialias=True,
     window=None,
@@ -23,7 +23,7 @@ def psychopy_circle(
     >>> import pyllusion as ill
     >>>
     >>> window = ill.psychopy_circle()
-    >>> window = ill.psychopy_circle(color="red", x=0.5, size=0.5)
+    >>> window = ill.psychopy_circle(color="red", color_outline="yellow", x=0.5, size=0.5)
     >>> window = ill.psychopy_circle(color="blue", x=-0.3, size=0.5, blur=0.05)
     >>> window = ill.psychopy_circle(color="yellow", y=0.5, blur=0.5)
 
@@ -39,10 +39,15 @@ def psychopy_circle(
     # Get coordinates
     radius, x, y = _coord_circle(image=win, diameter=size, x=x, y=y, method="psychopy")
     
+    # Border outline to be same as fill outline colour
+    if outline != 0:
+        if color_outline == "copy":
+            color_outline = color
+
     # Circle parameters
     circle = visual.Circle(win=win, radius=radius,
                            units="pix", fillColor=color, lineWidth=outline,
-                           lineColor=color_outline, edges=128)
+                           lineColor=color_outline, edges=radius*2, interpolate=antialias)
     circle.pos = [x-width/2, y-height/2]
     
     
