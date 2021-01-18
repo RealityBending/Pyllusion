2f309f6d8ef120e8c0810efd1056b40af37dea62
Zen <zen-juen@users.noreply.github.com>
2021-01-18 18:57:18 +0800

    refactor psychopy_text
    (check again, scaling using pygame as wintype seems off)

--- pyllusion/psychopy/psychopy_text.py
+++ pyllusion/psychopy/psychopy_text.py
@@ -1,60 +1,53 @@
 # -*- coding: utf-8 -*-
-from psychopy import core, event, visual
-
 from ..image.utilities import _coord_text
 
 
-def psychopy_text(window, text="Hello", width=500, height=500,
-                  x=0, y=0, size=10, color="black", background="white",
-                  font="arial.ttf", alpha=0, rotate=0, bold=False, italic=False):
+def psychopy_text(window, text="Hello",
+                  x=0, y=0, size=10, color="black", font="arial", alpha=0, rotate=0,
+                  bold=False, italic=False):
     """
     Examples
     --------
     >>> import pyllusion as ill
-    >>>
-    >>> window = ill.psychopy_text(text="Hello", size=40)  #doctest: +SKIP
-    >>> window = ill.psychopy_text(size=30, y=0.5, text="I'm Red", color="red")  #doctest: +SKIP
-    >>> window = ill.psychopy_text(size=20, x=0.5, bold=True, text="Bold and blurred", font="arialbd.ttf", alpha=0.5)  #doctest: +SKIP
+    >>> from psychopy import visual, event
 
+    >>> # Initiate window
+    >>> window = visual.Window(size=[500, 500], fullscr=False,
+                               screen=0, winType='pygame', monitor='testMonitor',
+                               allowGUI=False, color="white",
+                               blendMode='avg', units='pix')
+
+    >>> # Draw text
+    >>> ill.psychopy_text(window, size=40, text="Hello", font="arial.ttf")  #doctest: +SKIP
+
+    >>> # Refresh and close window
+    >>> window.flip()
+    >>> event.waitKeys()  # Press any key to close
+    >>> window.close()
     """
-    pass
 
+    # Try loading psychopy
+    try:
+        from psychopy import visual
+    except ImportError:
+        raise ImportError(
+            "The 'psychopy' module is required for this function to run. ",
+            "Please install it first (`pip install PsychoPy`).",
+        )
 
-    # # Try loading mne
-    # try:
-    #     import psychopy
-    # except ImportError:
-    #     raise ImportError(
-    #         "The 'psychopy' module is required for this function to run. ",
-    #         "Please install it first (`pip install PsychoPy`).",
-    #     )
+    # Get coordinates
+    message = visual.TextStim(win=window, text=text, font=font, bold=bold, italic=italic,
+                              color=color, units='pix', height=size)
+    _, _, x, y = _coord_text(window, text=text, size=size, x=x, y=y, font=font, method="psychopy")
+    message.pos = [x-window.size[0]/2, y-window.size[1]/2]
 
-    # # Draw window
-    # win = visual.Window(size=[width, height], fullscr=full_screen,
-    #                 screen=0, winType='pyglet', allowGUI=False,
-    #                 allowStencil=False,
-    #                 monitor='testMonitor', color=background, colorSpace='rgb',
-    #                 blendMode='avg', units='pix')
+    # Alpha
+    if alpha > 0:
+        message.opacity = alpha
 
-    # # Get coordinates
-    # message = visual.TextStim(win=win, text=text, font=font, bold=bold, italic=italic,
-    #                           color=color, units='pix', height=size)
-    # _, _, x, y = _coord_text(win, text=text, size=size, x=x, y=y, font=font, method="psychopy")
-    # message.pos = [x-width/2, y-height/2]
+    # Orientation
+    if rotate != 0:
+        message.ori = rotate
 
-    # # Alpha
-    # if alpha > 0:
-    #     message.opacity = alpha
-
-    # # Orientation
-    # if rotate != 0:
-    #     message.ori = rotate
-
-    # # Display
-    # while True:
-    #     message.draw()
-    #     win.flip()
-
-    #     if 'escape' in event.getKeys():
-    #         win.close()
-    #         core.quit()
+    # Display message
+    message.draw()
