2a6a8f7bf06ca2d0c05d175a9b8cc11257895818
Zen <zen-juen@users.noreply.github.com>
2021-01-18 19:18:08 +0800

    refactor psychopy_line

--- pyllusion/psychopy/psychopy_line.py
+++ pyllusion/psychopy/psychopy_line.py
@@ -1,33 +1,47 @@
 # -*- coding: utf-8 -*-
 from ..image.utilities import _coord_line
-from psychopy import visual, event, core
 
 
-def psychopy_line(width=800, height=600, x=0, y=0, x1=None, y1=None, x2=None, y2=None, length=1,
-                  rotate=0, size=1, color="black", background="white", window=None,
-                  blur=0, full_screen=False, adjust_width=False, adjust_height=False):
+def psychopy_line(window, x=0, y=0, x1=None, y1=None, x2=None, y2=None, length=1,
+                  rotate=0, size=1, color="black", blur=0,
+                  adjust_width=False, adjust_height=False):
     """
     Examples
     --------
     >>> import pyllusion as ill
-    >>>
-    >>> window = ill.psychopy_line(x=0, y=0, length=1)
-    """
+    >>> from psychopy import visual, event
+    
+    >>> # Initiate window
+    >>> window = visual.Window(size=[800, 600], fullscr=False,
+                               screen=0, winType='pygame', monitor='testMonitor',
+                               allowGUI=False, allowStencil=False, 
+                               color="white", colorSpace='rgb',
+                               blendMode='avg', useFBO=True, units='norm')
 
-    # Draw window
-    win = visual.Window(size=[width, height], fullscr=full_screen,
-                        screen=0, winType='pyglet', allowGUI=False,
-                        allowStencil=False,
-                        monitor='testMonitor', color=background, colorSpace='rgb',
-                        blendMode='avg', useFBO=True, units='norm')
+    >>> # Draw line
+    >>> ill.psychopy_line(window, x=0, y=0, length=1)
+    
+    >>> # Refresh and close window    
+    >>> window.flip()
+    >>> event.waitKeys()  # Press any key to close
+    >>> window.close()
+    """
+    # Try loading psychopy
+    try:
+        from psychopy import visual
+    except ImportError:
+        raise ImportError(
+            "The 'psychopy' module is required for this function to run. ",
+            "Please install it first (`pip install PsychoPy`).",
+        )
 
     # Get coordinates
-    coord, leng, angle = _coord_line(image=window, x=x, y=y, x1=x1, y1=y1, x2=x2, y2=y2,
+    coord, leng, angle = _coord_line(image=None, x=x, y=y, x1=x1, y1=y1, x2=x2, y2=y2,
                                      length=length, angle=rotate,
                                      adjust_width=adjust_width, adjust_height=adjust_height)
 
     # Line parameters
-    line = visual.Line(win=win, units='norm', lineColor=color, lineWidth=size)
+    line = visual.Line(win=window, units='norm', lineColor=color, lineWidth=size)
     line.start = [coord[0], coord[1]]
     line.end = [coord[2], coord[3]]
     
@@ -36,11 +50,4 @@ def psychopy_line(width=800, height=600, x=0, y=0, x1=None, y1=None, x2=None, y2
         line.opacity = blur
     
     # Display
-    while True:
-        line.draw()
-        win.flip()
-    
-        if 'escape' in event.getKeys():
-            win.close()
-            core.quit()
-    
+    line.draw()
