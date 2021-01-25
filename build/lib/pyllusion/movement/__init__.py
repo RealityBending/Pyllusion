388e85b6ff1ade972a89867e5a0a8d814e03261c
Dominique Makowski <dom.mak19@gmail.com>
2021-01-25 11:51:14 +0800

    fix action doc

--- build/lib/pyllusion/movement/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Pyllusion submodule.
-"""
-
-
-from .movement_matrix import movement_matrix
-from .movement_circles import movement_circles
-from .utilities import images_to_gif
-from .motiontransparency import motiontransparency_parameters, motiontransparency_images
