23e3cc2e864a5cde56ad42c74fdb8f0eb590c6f6
Zen <zen-juen@users.noreply.github.com>
2021-02-02 16:08:49 +0800

    new gifs in readme

---
+++ docs/example_docs.py
@@ -0,0 +1,71 @@
+# Import packages
+import pyllusion as ill
+import numpy as np
+from psychopy import visual, event, gui, core
+import wx
+
+# Set up GUI
+gui = gui.Dlg()
+gui.addField("Subject ID:")
+gui.addField("Experiment Title:")
+gui.show()
+
+# Initiate window
+app = wx.App(False)
+width = wx.GetDisplaySize()[0]
+height = wx.GetDisplaySize()[1]
+window = visual.Window(size=[width-100, height-100], fullscr=False,
+                       screen=0, winType='pyglet', monitor='testMonitor',
+                       allowGUI=False, color="white",
+                       blendMode='avg', units='pix')
+
+# Start instructions
+instructions = visual.TextStim(win=window, wrapWidth=350, color="black")
+instructions.text = """You will now see some visual illusions.\n
+Press any key to begin."""
+instructions.draw()
+window.flip()
+event.waitKeys()
+
+# Create fixation cross
+fix = visual.TextStim(win=window, name='fix', text='+',
+                      font='Arial',
+                      pos=(0, 0), height=100, wrapWidth=None, ori=0, 
+                      color='black', colorSpace='rgb', opacity=1, 
+                      languageStyle='LTR',
+                      depth=-1.0)
+jitter_duration = np.arange(0.5, 1.5, .15)
+
+# Initiate parameters for illusion and draw 
+differences = np.random.randint(low=1, high=6, size=(5,))
+strengths = np.random.randint(low=1, high=6, size=(5,))
+parameters_list = []
+
+for diff, strg in zip(differences, strengths):
+    parameters = ill.ebbinghaus_parameters(difference=diff, illusion_strength=strg)
+    parameters_list.append(parameters)
+
+for param in parameters_list:  # Loop illusions to display
+
+    # Fixation Cross
+    jitter = np.random.choice(jitter_duration)
+    fix.draw()
+    window.flip()
+    core.wait(jitter)
+
+    # Display illusion
+    ill.ebbinghaus_psychopy(window=window, parameters=param)
+    window.flip()
+    event.waitKeys()  # Press any key to move on
+ 
+message_end = visual.TextStim(win=window, wrapWidth=350, color="black")
+message_end.text = """You have now come to the end of the experiment.
+Press any key to end.
+"""
+message_end.draw()
+window.flip()
+event.waitKeys()
+
+window.close()
+
+
--- docs/example_gif.py
+++ docs/example_gif.py
@@ -1,78 +1,17 @@
-# -*- coding: utf-8 -*-
-# Import packages
+# Load packages
 import pyllusion as ill
-import numpy as np
-from psychopy import visual, event, gui
-import os
-import sys
-import wx
+from psychopy import visual, event
 
-# Set up GUI
-gui = gui.Dlg()
-gui.addField("Subject ID:")
-gui.addField("Experiment Title:")
+# Create parameters
+parameters = ill.delboeuf_parameters(illusion_strength=1, difference=2)
 
-gui.show()
+# Initiate Window
+window = visual.Window(size=[1820, 980], winType='pygame', color='white', fullscr=False)
 
-# subj_id = gui.data[0]
-# rep_num = gui.data[1]
+# Display illusion
+ill.delboeuf_psychopy(window=window, parameters=parameters)
 
-# data_path = subj_id + "_rep_" + rep_num + ".tsv"
-
-# if os.path.exists(data_path):
-#     sys.exit("Data path " + data_path + " already exists!")
-# exp_data = []
-
-# Initiate window
-app = wx.App(False)
-width = wx.GetDisplaySize()[0]
-height = wx.GetDisplaySize()[1]
-
-window = visual.Window(size=[width-100, height-100], fullscr=False,
-                       screen=0, winType='pyglet', monitor='testMonitor',
-                       allowGUI=False, color="white",
-                       blendMode='avg', units='pix')
-
-# # Set up mouse event
-# mouse = event.Mouse(visible=True, win=window)
-
-# Start instructions
-instructions = visual.TextStim(win=window, wrapWidth=350, color="black")
-
-instructions.text = """
-You will now see some visual illusions.\n
-Press any key to begin.
-"""
-instructions.draw()
+# Refresh and close window
 window.flip()
-event.waitKeys()
-
-# Initiate parameters for illusion and draw 
-
-difficulty = np.random.randint(low=1, high=6, size=(5,))
-strength = np.random.randint(low=1, high=6, size=(5,))
-parameters_list = []
-
-for diff, strg in zip(difficulty, strength):
-    parameters = ill.ebbinghaus_parameters(difficulty=diff, illusion_strength=strg)
-    parameters_list.append(parameters)
-
-for param in parameters_list:  # Loop illusions to display
-    # print(param)
-    ill.ebbinghaus_psychopy(window=window, parameters=param)
-    window.flip()
-    event.waitKeys()  # Press any key to move on
- 
-message_end = visual.TextStim(win=window, wrapWidth=350, color="black")
-
-message_end.text = """
-You have now come to the end of the experiment.
-Press any key to end.
-"""
-message_end.draw()
-window.flip()
-event.waitKeys()
-
+event.waitKeys()  # Press any key to close
 window.close()
-
-
