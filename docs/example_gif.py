c6092d122e3816affc9caad2b3b4b5d7a2028c0d
Zen <lauzenjuen@gmail.com>
2021-07-30 11:29:24 +0800

    update readme and gif

--- docs/example_gif.py
+++ docs/example_gif.py
@@ -3,13 +3,14 @@ import pyllusion
 from psychopy import visual, event
 
 # Create parameters
-parameters = pyllusion.delboeuf_parameters(illusion_strength=1, difference=2)
+delboeuf = pyllusion.Delboeuf(illusion_strength=1, difference=2)
 
 # Initiate Window
-window = visual.Window(size=[1820, 980], winType='pygame', color='white', fullscr=False)
+window = visual.Window(size=[1920, 1080], winType='pygame',
+                       color='white', fullscr=False)
 
 # Display illusion
-pyllusion.delboeuf_psychopy(window=window, parameters=parameters)
+delboeuf.to_psychopy(window)
 
 # Refresh and close window
 window.flip()
