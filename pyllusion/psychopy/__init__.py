082736a230d8c91ab3d96559b84249dfa5c7d58d
Tam-Pham <phamttam17@gmail.com>
2021-08-04 11:51:16 +0800

    allow documentation for psychopy module

--- pyllusion/psychopy/__init__.py
+++ pyllusion/psychopy/__init__.py
@@ -1,6 +1,12 @@
 """
 Pyllusion submodule.
 """
-from .psychopy_line import psychopy_line
 from .psychopy_circle import psychopy_circle
+from .psychopy_line import psychopy_line
 from .psychopy_rectangle import psychopy_rectangle
+
+__all__ = [
+    "psychopy_line",
+    "psychopy_circle",
+    "psychopy_rectangle"
+]
